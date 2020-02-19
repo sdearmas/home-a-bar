50ab896225b33555572dc3b5dec20af6d6de763a
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-02-18 23:30:43 -0800

    HI

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@ $(function() {
   $(".nav-list").draggable();
 
 
-
-
+  for (var i=1; i <= 3; i++) {
+  	var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+  	var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+  	$('.nav-list').css({
+  		top: y + '%',
+  		left: x + '%',
+  	});
+  }
 
 
 
