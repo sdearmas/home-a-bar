9e76e845008bb1b9d9a32fad60ea58859fe05a0f
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-02-20 10:16:31 -0800

    Nav

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
   $(".nav-list").draggable();
 
-
   for (var i=1; i <= 3; i++) {
   	var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
   	var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
