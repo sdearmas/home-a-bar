02a3582f8b9764dc089874955a63b141e35230f8
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-03-11 20:52:01 -0700

    ALMOST V2

--- js/script.js
+++ js/script.js
@@ -4,14 +4,27 @@ $(function() {
   $(".nav-list").draggable();
 
   for (var i=1; i <= 3; i++) {
-  	var x = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
-  	var y = Math.max(0, Math.min(100, Math.ceil(Math.random() * 100)));
-    var d = Math.floor(Math.random() * Math.floor (360));
+  	var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+  	var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+    var d = Math.floor(Math.random() * Math.floor (180));
   	$('.nav-list').css({
   		top: y + '%',
   		left: x + '%',
       transform: 'rotate(' + d + 'deg)'
   	});
+
+    console.log(d)
+  }
+
+  for (var i=1; i <= 3; i++) {
+    var x = Math.max(0, Math.min(80, Math.ceil(Math.random() * 100)));
+    var y = Math.max(0, Math.min(800, Math.ceil(Math.random() * 1000)));
+    var d = Math.floor(Math.random() * Math.floor (360));
+    $('.nav-list-menu').css({
+      top: y + '%',
+      left: x + '%',
+      transform: 'rotate(' + d + 'deg)'
+    });
 
     console.log(d)
   }
