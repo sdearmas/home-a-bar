9214e61409398147d740abc13b50a8e08208f58e
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-03-12 13:57:42 -0700

    H

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   $(".nav-list").draggable();
-  $(".nav-list-menu").draggable();
 
   for (var i=1; i <= 3; i++) {
   	var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
-  	var y = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+  	var y = Math.max(0, Math.min(70, Math.ceil(Math.random() * 100)));
     var d = Math.floor(Math.random() * Math.floor (180));
   	$('.nav-list').css({
   		top: y + '%',
@@ -15,20 +14,6 @@ $(function() {
   	});
 
     console.log(d)
-  }
-
-  for (var i=1; i <= 3; i++) {
-    var x = Math.max(0, Math.min(80, Math.ceil(Math.random() * 100)));
-    var y = Math.max(0, Math.min(800, Math.ceil(Math.random() * 1000)));
-    var d = Math.floor(Math.random() * Math.floor (360));
-    $('.nav-list-menu').css({
-      top: y + '%',
-      left: x + '%',
-      transform: 'rotate(' + d + 'deg)'
-    });
-
-    console.log(d)
-  }
 
 
 
