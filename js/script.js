0d68b05fca8ac4e6407ea100cff822d789be3327
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-03-17 02:06:50 -0700

    Wut

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(function() {
   $(".nav-list").draggable();
 
   for (var i=1; i <= 3; i++) {
-  	var x = Math.max(10, Math.min(60, Math.ceil(Math.random() * 100)));
-  	var y = Math.max(10, Math.min(60, Math.ceil(Math.random() * 100)));
+  	var x = Math.max(0, Math.min(60, Math.ceil(Math.random() * 100)));
+  	var y = Math.max(0, Math.min(70, Math.ceil(Math.random() * 100)));
     var d = Math.floor(Math.random() * Math.floor (180));
   	$('.nav-list').css({
   		top: y + '%',
@@ -13,8 +13,21 @@ $(function() {
       transform: 'rotate(' + d + 'deg)'
   	});
 
-    console.log(d)
+    console.log(d)  }
 
+    $(".nav-list-menu").draggable();
+
+    for (var i=1; i <= 3; i++) {
+    	var x = Math.max(0, Math.min(65, Math.ceil(Math.random() * 100)));
+    	var y = Math.max(0, Math.min(250, Math.ceil(Math.random() * 100)));
+      var d = Math.floor(Math.random() * Math.floor (180));
+    	$('.nav-list-menu').css({
+    		top: y + '%',
+    		left: x + '%',
+        transform: 'rotate(' + d + 'deg)'
+    	});
+
+      console.log(d)  }
 
 
 
